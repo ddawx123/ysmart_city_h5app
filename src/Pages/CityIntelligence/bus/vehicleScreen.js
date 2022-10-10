6cf5384603f43ccfcb659e1e50a715762172429f
David Ding <ding@dscitech.com>
2022-10-10 11:39:36 +0800

    补全配色方案

--- src/Pages/CityIntelligence/bus/vehicleScreen.js
+++ src/Pages/CityIntelligence/bus/vehicleScreen.js
@@ -6,7 +6,7 @@ export default function VehicleScreen(props) {
             return (
                 <div className="weui-media-box weui-media-box_text" key={i} style={{textAlign: 'left'}}>
                     <div className="weui-media-box__title weui-media-box__title_in-text">{v.name}：{v.info}</div>
-                    <div className="weui-media-box__desc display-type_{v.colorType}">{v.time}</div>
+                    <div className={"weui-media-box__desc display-type_"+v.colorType}>{v.time}</div>
                 </div>
             )
         })
